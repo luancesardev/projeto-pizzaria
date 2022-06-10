8067c362ebfe3b36c8be5d1aa688a21716e70a0c
LuaanBarbosa <luancesar@outlook.com>
2022-06-10 16:47:01 -0300

    Funções do modal
    Construção das funcionalidades do modal.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
-const s = (qs) =>document.querySelector(qs);
-const a = (qa) =>document.querySelectorAll(qa);
+let s = (qs) =>document.querySelector(qs);
+let a = (qa) =>document.querySelectorAll(qa);
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = s('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('date-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML =  `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        let key = e.target.closest('.pizza-item').getAttribute('date-key');
+
+        s('.pizzaBig img').src = pizzaJson[key].img;
+        s('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        s('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        s('.pizzaInfo--actualPrice').innerHTML =  `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        a('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+
+
+        s('.pizzaWindowArea').style.opacity = '0';
+        s('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            s('.pizzaWindowArea').style.opacity = '1';
+        },200);
+        
+    });
 
     s('.pizza-area').append(pizzaItem);
 
 });
+
+
