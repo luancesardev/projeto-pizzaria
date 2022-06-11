8b5c803f58e30d111360e037400d938bd902da65
luancesardev <luancesar@outlook.com>
2022-06-10 23:26:10 -0300

    Alterações nas cores do layout

--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -1,11 +1,11 @@
 let pizzaJson = [
-    {id:1, name:'Moda da Casa', img:'assets/images/pizza.png', price:34.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, frango, tomate, palmito e azeitonas pretas'},
-    {id:2, name:'Marguerita', img:'assets/images/pizza2.png', price:34.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, manjericão e tomate'},
-    {id:3, name:'Gaúcha', img:'assets/images/pizza3.png', price:34.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, filé mignon, pimentões e azeitonas pretas'},
-    {id:4, name:'Vegetariana', img:'assets/images/pizza4.png', price:34.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, tomate, brócolis e azeitonas pretas'},
-    {id:5, name:'Calabresa Especial', img:'assets/images/pizza5.png', price:34.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, calabresa, tomates, catupiry, azeitonas verdes e pretas'},
-    {id:6, name:'Moda do Chef', img:'assets/images/pizza6.png', price:34.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, frango, bacon, tomate, manjericão e azeitonas pretas'},
-    {id:7, name:'M & Ms', img:'assets/images/pizza7.png', price:34.90, sizes:['320g', '530g', '860g'], description:'Mussarela, chocolate e m&ms'},
-    {id:8, name:'Prestígio', img:'assets/images/pizza8.png', price:34.90, sizes:['320g', '530g', '860g'], description:'Mussarela, chocolate e coco ralado'},
-    {id:9, name:'Abacaxi Chocolate', img:'assets/images/pizza9.png', price:34.90, sizes:['320g', '530g', '860g'], description:'Mussarela, abacaxi e chocolate branco'}
+    {id:1, name:'Moda da Casa', img:'assets/images/pizza.png', price:23.92, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, frango, tomate, palmito e azeitonas pretas'},
+    {id:2, name:'Marguerita', img:'assets/images/pizza2.png', price:23.92, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, manjericão e tomate'},
+    {id:3, name:'Gaúcha', img:'assets/images/pizza3.png', price:29.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, filé mignon, pimentões e azeitonas pretas'},
+    {id:4, name:'Vegetariana', img:'assets/images/pizza4.png', price:29.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, tomate, brócolis e azeitonas pretas'},
+    {id:5, name:'Calabresa Especial', img:'assets/images/pizza5.png', price:29.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, calabresa, tomates, catupiry, azeitonas verdes e pretas'},
+    {id:6, name:'Moda do Chef', img:'assets/images/pizza6.png', price:29.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, frango, bacon, tomate, manjericão e azeitonas pretas'},
+    {id:7, name:'M & Ms', img:'assets/images/pizza7.png', price:29.90, sizes:['320g', '530g', '860g'], description:'Mussarela, chocolate e m&ms'},
+    {id:8, name:'Prestígio', img:'assets/images/pizza8.png', price:29.90, sizes:['320g', '530g', '860g'], description:'Mussarela, chocolate e coco ralado'},
+    {id:9, name:'Abacaxi Chocolate', img:'assets/images/pizza9.png', price:29.90, sizes:['320g', '530g', '860g'], description:'Mussarela, abacaxi e chocolate branco'}
 ];
